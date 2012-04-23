9413c0739d8af8d9e29a82461a6cc43cce6cac85
zwheeler <zwheeler@localhost>
2012-04-23 21:54:36 +0000

    LEARNING THE TOOLS NOOBS.

---
+++ 131Final/131Final/131Final/Engine/Base/Sprite.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Base
+{
+    public class BaseSprite : GameObject
+    {
+        /*Sprite Vars:*/
+        protected String _TextureName;
+        protected Texture2D _SpriteTexture;
+        protected Vector2 _SpritePos;
+        protected Color _SpriteTint;
+        /*Game Vars*/
+        protected ContentManager gameContent;
+        protected SpriteBatch gameSpriteBatch;
+
+        /*Constructer*/
+        public BaseSprite(ContentManager GC, SpriteBatch GSB, String Texture, Vector2 Pos, Color color)
+        {
+            Tint = color;
+            myPos = Pos;
+            _TextureName = Texture;
+            gameContent = GC;
+            gameSpriteBatch = GSB;
+            Load();
+        }
+        public BaseSprite(Texture2D myTexture, Vector2 myPos, Color color)
+        {
+
+        }
+        /*Personal Methods*/
+        public override void Draw(GameTime gameTime)
+        {
+            gameSpriteBatch.Draw(_SpriteTexture, _SpritePos, _SpriteTint);
+        }
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+        private void Load()
+        {
+            _SpriteTexture = gameContent.Load<Texture2D>(_TextureName);
+        }
+        /*Manipulators*/
+        public Color Tint
+        {
+            set
+            {
+                _SpriteTint = value;
+            }
+            get
+            {
+                return _SpriteTint;
+            }
+        }
+        public float X
+        {
+            set
+            {
+                _SpritePos.X = value;
+            }
+            get
+            {
+                return _SpritePos.X;
+            }
+        }
+        public float Y
+        {
+            set
+            {
+                _SpritePos.Y = value;
+            }
+            get
+            {
+                return _SpritePos.Y;
+            }
+        }
+        public Vector2 myPos
+        {
+            set
+            {
+                _SpritePos = value;
+            }
+            get
+            {
+                return _SpritePos;
+            }
+        }
+        public virtual Vector2 Size
+        {
+            get
+            {
+                return new Vector2(_SpriteTexture.Width, _SpriteTexture.Height);
+            }
+        }
+    }
+}
---
+++ 131Final/131Final/131Final/Engine/GameVars/SystemVars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine
+{
+    public struct SystemVars
+    {
+        public static bool DEBUG = false;
+        public static double DefenseEffect = 50;
+        public static double mDefenseEffect = 100;
+        //Yeppers :3
+
+
+    }
+}
